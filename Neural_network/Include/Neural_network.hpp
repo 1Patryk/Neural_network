d2e9122dcaefdc59a6d0f1d2d1650b8f6f9d3d5a
Patryk Polak <patryk14163@interia.eu>
2023-10-22 10:32:40 +0200

    Adding separate folders 'Include' and 'Source'

---
+++ Neural_network/Include/Neural_network.hpp
@@ -0,0 +1,6 @@
+﻿#ifndef NEURAL_NETWORK_HPP		// If is not declarate/added this header file
+#define NEURAL_NETWORK_HPP		// declarating/adding him
+
+#include <iostream>				// adding standard input/output library
+
+#endif							// end if function of NEURAL_NETWORK.HPP
